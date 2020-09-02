5c9c5ef3e36c1e60fc0966131f4089126941300f
Rubin <rubinradhakrishnan@gmail.com>
2020-09-02 20:18:22 +0530

    Add icons in the header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBQJNP6eg0zPB-e8ogGus6PL504KpocsM",
+  authDomain: "facebook-clone-88c97.firebaseapp.com",
+  databaseURL: "https://facebook-clone-88c97.firebaseio.com",
+  projectId: "facebook-clone-88c97",
+  storageBucket: "facebook-clone-88c97.appspot.com",
+  messagingSenderId: "596962835198",
+  appId: "1:596962835198:web:4b928a202ca8545d9c83af",
+  measurementId: "G-R8C4JBMPGX",
+};
