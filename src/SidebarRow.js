f574bfcddbea44d2cc3d08d826de53b4eecd2866
Rubin <rubinradhakrishnan@gmail.com>
2020-09-02 23:56:53 +0530

    Add title to the searchbarRow elements

--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -7,6 +7,8 @@ function SidebarRow({ src, Icon, title }) {
     <div className="sidebarRow">
       {src && <Avatar src={src} />}
       {Icon && <Icon />}
+
+      <h4>{title}</h4>
     </div>
   );
 }
