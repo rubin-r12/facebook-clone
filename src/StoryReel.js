b35f6c7859ca8f605f49c485c385ead18fd76148
Rubin <rubinradhakrishnan@gmail.com>
2020-09-03 01:29:29 +0530

    Add StoryReel to the Feed component

---
+++ src/StoryReel.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./StoryReel.css";
+
+function StoryReel() {
+  return (
+    <div className="storyReel">
+      <h2>I'm a story Reel</h2>
+    </div>
+  );
+}
+
+export default StoryReel;
