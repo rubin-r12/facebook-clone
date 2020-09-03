f2c0c98e5f9abb9572445de4f8a8170e55b160cf
Rubin <rubinradhakrishnan@gmail.com>
2020-09-04 05:00:20 +0530

    Deploy the facebook-clone

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -5,6 +5,8 @@ import VideocamIcon from "@material-ui/icons/Videocam";
 import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import { useStateValue } from "./StateProvider";
+import db from "./firebase";
+import firebase from "firebase";
 
 function MessageSender() {
   const [{ user }, dispatch] = useStateValue();
@@ -14,7 +16,13 @@ function MessageSender() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    //some clever db stuff
+    db.collection("posts").add({
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      profilePic: user.photoURL,
+      username: user.displayName,
+      image: imageUrl,
+    });
 
     setInput("");
     setImageUrl("");
