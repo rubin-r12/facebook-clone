8145341ee32e43ce398ca82c2a60c9eaae8a4adb
Rubin <rubinradhakrishnan@gmail.com>
2020-09-04 00:36:49 +0530

    Add Login component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
+import Login from "./Login";
 
 function App() {
+  const user = "Rubin";
+
   return (
     <div className="app">
-      <Header />
-
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
     </div>
   );
 }
