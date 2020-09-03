0503125d598953ba44c7869a6ffc4bac844aeda4
Rubin <rubinradhakrishnan@gmail.com>
2020-09-03 18:16:42 +0530

    Add useState to the Input and ImageUrl fields

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MessageSender.css";
 import { Avatar } from "@material-ui/core";
 import VideocamIcon from "@material-ui/icons/Videocam";
@@ -6,8 +6,16 @@ import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 
 function MessageSender() {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    //some clever db stuff
+
+    setInput("");
+    setImageUrl("");
   };
 
   return (
@@ -16,10 +24,16 @@ function MessageSender() {
         <Avatar />
         <form>
           <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
             className="messageSender__input"
             placeholder="What's on your mind?"
           />
-          <input placeholder="Image URL(optional)" />
+          <input
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            placeholder="Image URL(optional)"
+          />
           <button onClick={handleSubmit} type="submit">
             Hidden submit
           </button>
