56886a39f5d933e39a0eac4cf666b7094addf9ac
Rubin <rubinradhakrishnan@gmail.com>
2020-09-04 01:30:45 +0530

    Add Google Authentication

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Login.css";
 import { Button } from "@material-ui/core";
+import { auth, provider } from "./firebase";
 
 function Login() {
   const signIn = () => {
     //sign in...
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => alert(error.message));
   };
 
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBBQJNP6eg0zPB-e8ogGus6PL504KpocsM",
   authDomain: "facebook-clone-88c97.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
   appId: "1:596962835198:web:4b928a202ca8545d9c83af",
   measurementId: "G-R8C4JBMPGX",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
