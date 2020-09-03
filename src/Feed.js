2ba235eafc16cf0f4d35034f650b05c95a45b8e0
Rubin <rubinradhakrishnan@gmail.com>
2020-09-03 23:09:12 +0530

    Add the like, comment, share buttons beneath the post

--- src/Feed.js
+++ src/Feed.js
@@ -2,12 +2,22 @@ import React from "react";
 import "./Feed.css";
 import StoryReel from "./StoryReel";
 import MessageSender from "./MessageSender";
+import Post from "./Post";
 
 function Feed() {
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
+      <Post
+        profilePic="https://i.ibb.co/bv02CBw/IIIT-hyd.jpg"
+        message="Wow! This works!"
+        timestamp="This is a timestamp"
+        username="rubin-r12"
+        image="https://i.ibb.co/JRC4Xsk/haley-carman-c4-Lp-Yfiz-Lvw-unsplash.jpg"
+      />
+      <Post />
+      <Post />
     </div>
   );
 }
